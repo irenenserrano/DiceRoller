cbfdd4697762c64df9db62afd634b0b041a33c13
iserrano <iserrano@mills.edu>
2021-02-19 12:01:04 -0800

    Implement Timber logging API

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import timber.log.Timber
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +20,14 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = getString(R.string.roll_button_text)
         rollButton.setOnClickListener {
             rollDice()
+            Timber.i("Die rolled")
         }
+
 
         val decreaseButton: Button = findViewById(R.id.decrease_button)
         decreaseButton.text = getString(R.string.decrease_maximum_value, numSides - 1)
-        decreaseButton.setOnClickListener{
+        decreaseButton.setOnClickListener {
+            Timber.i("MAX VALUE DECREASED; NUM SIDES NOW $numSides")
             decreaseMaxValue()
             if (numSides == 1){
                 decreaseButton.visibility = View.INVISIBLE
@@ -46,7 +52,53 @@ class MainActivity : AppCompatActivity() {
         DICE_IMAGE.setImageResource(drawableResource)
     }
 
+    private fun retrieveImage(dieValue :Int) {
+        val drawableResource = when(dieValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        DICE_IMAGE.setImageResource(drawableResource)
+    }
     private fun decreaseMaxValue() {
         numSides = if (numSides == 1) 1 else numSides - 1
+    }
+    
+    override fun onStart() {
+        super.onStart()
+        Timber.i("APP STARTED")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+        Timber.i("APP RESUMED")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+        Timber.i("APP PAUSED")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+        Timber.i("APP STOPPED")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+        Timber.i("APP DESTROYED")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart called")
+        Timber.i("APP RESTARTED")
     }
 }
